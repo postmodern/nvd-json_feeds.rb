6072e6e820bf7287948ef2a6ea999977c6f67787
Postmodern <postmodern.mod3@gmail.com>
2021-01-19 07:56:37 -0800

    Added specs for GzFeedURI and ZipFeedURI.

---
+++ spec/gz_feed_uri_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+require 'nvd/json_feeds/gz_feed_uri'
+
+describe NVD::JSONFeeds::GzFeedURI do
+  let(:fixtures_dir) { File.expand_path('../fixtures',__FILE__) }
+
+  let(:name) { :recent }
+  let(:ext)  { '.json.gz' }
+  
+  subject { described_class.new(name,ext) }
+
+  describe "#download", :integration do
+    let(:download_dir) { File.join(fixtures_dir,'download') }
+    let(:dest) { File.join(download_dir,subject.filename) }
+
+    before do
+      FileUtils.mkdir_p(download_dir)
+      FileUtils.rm_f(dest)
+    end
+
+    it "must return a GzFeedFile object for the newly downloaded file" do
+      feed_file = subject.download(dest)
+
+      expect(feed_file).to be_kind_of(GzFeedFile)
+      expect(feed_file.path).to eq(dest)
+      expect(File.file?(dest)).to be(true)
+    end
+
+    after do
+      FileUtils.rm_f(dest)
+    end
+  end
+end
