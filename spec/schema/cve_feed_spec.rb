1b20e42d1259a7ae9d2248d942d4de915a5119b6
Postmodern <postmodern.mod3@gmail.com>
2021-01-20 11:22:34 -0800

    DRYed up JSON parsing specs.

--- spec/schema/cve_feed_spec.rb
+++ spec/schema/cve_feed_spec.rb
@@ -11,119 +11,33 @@ describe NVD::JSONFeeds::Schema::CVEFeed do
 
     it { expect(subject).to be_kind_of(described_class) }
 
-    context '"CVE_date_type"' do
-      let(:json_value) { json_node['CVE_data_type'] }
-      let(:expected)   { described_class::DATA_TYPES[json_value] }
+    include_examples "JSON field", json_key: 'CVE_data_type',
+                                   required: true,
+                                   method:   :data_type,
+                                   map:      described_class::DATA_TYPES
 
-      it "must set #data_type" do
-        expect(subject.data_type).to eq(expected)
-      end
+    include_examples "JSON field", json_key: 'CVE_data_format',
+                                   required: true,
+                                   method:   :data_format,
+                                   map:      described_class::DATA_FORMATS
 
-      context 'when missing' do
-        before { json_node.delete('CVE_data_type') }
+    include_examples "JSON field", json_key: 'CVE_data_version',
+                                   required: true,
+                                   method:   :data_version,
+                                   map:      described_class::DATA_VERSIONS
 
-        it do
-          expect {
-            described_class.load(json_node)
-          }.to raise_error(KeyError)
-        end
-      end
-    end
+    include_examples "JSON field", json_key: 'CVE_data_numberOfCVEs',
+                                   required: true,
+                                   method:   :data_number_of_cves,
+                                   value:    ->{ json_value.to_i }
 
-    context '"CVE_date_format"' do
-      let(:json_value) { json_node['CVE_data_format'] }
-      let(:expected)   { described_class::DATA_FORMATS[json_value] }
+    include_examples "JSON field", json_key: 'CVE_data_timestamp',
+                                   method:   :data_timestamp,
+                                   value:    ->{ Schema::Timestamp.parse(json_value) }
 
-      it "must set #data_format" do
-        expect(subject.data_format).to eq(expected)
-      end
-
-      context 'when missing' do
-        before { json_node.delete('CVE_data_format') }
-
-        it do
-          expect {
-            described_class.load(json_node)
-          }.to raise_error(KeyError)
-        end
-      end
-    end
-
-    context '"CVE_data_version"' do
-      let(:json_value) { json_node['CVE_data_version'] }
-      let(:expected)   { Schema::HasDataVersion::DATA_VERSIONS[json_value] }
-
-      it "must set #data_version" do
-        expect(subject.data_version).to eq(expected)
-      end
-
-      context 'when missing' do
-        before { json_node.delete('CVE_data_version') }
-
-        it do
-          expect {
-            described_class.load(json_node)
-          }.to raise_error(KeyError)
-        end
-      end
-    end
-
-    context '"CVE_data_numberOfCVEs"' do
-      let(:json_value) { json_node['CVE_data_numberOfCVEs'] }
-      let(:expected)   { json_value.to_i }
-
-      it "must set #data_number_of_cves" do
-        expect(subject.data_number_of_cves).to eq(expected)
-      end
-
-      context 'when missing' do
-        before { json_node.delete('CVE_data_numberOfCVEs') }
-
-        it do
-          expect {
-            described_class.load(json_node)
-          }.to raise_error(KeyError)
-        end
-      end
-    end
-
-    context '"CVE_data_timestamp"' do
-      context "when present" do
-        let(:json_value) { json_node['CVE_data_timestamp']     }
-        let(:expected)   { Schema::Timestamp.parse(json_value) }
-
-        it "must parse and set #data_timestamp" do
-          expect(subject.data_timestamp).to eq(expected)
-        end
-      end
-
-      context "when missing" do
-        before { json_node.delete('CVE_data_timestamp') }
-
-        it { expect(subject.data_timestamp).to be(nil) }
-      end
-    end
-
-    context '"CVE_Items"' do
-      let(:json_value) { json_node['CVE_Items'] }
-
-      it { expect(subject.cve_items).to be_kind_of(Array) }
-      it { expect(subject.cve_items).to_not be_empty }
-      it { expect(subject.cve_items).to all(be_kind_of(Schema::CVEItem)) }
-
-      it 'must parse all elements of "CVE_Items"' do
-        expect(subject.cve_items.length).to eq(json_value.length)
-      end
-
-      context "when missing" do
-        before { json_node.delete('CVE_Items') }
-
-        it do
-          expect {
-            described_class.load(json_node)
-          }.to raise_error(KeyError)
-        end
-      end
-    end
+    include_examples "JSON Array field", json_key:      'CVE_Items',
+                                         method:        :cve_items,
+                                         required:      true,
+                                         element_class: Schema::CVEItem
   end
 end
--- spec/schema/shared_examples.rb
+++ spec/schema/shared_examples.rb
@@ -17,3 +17,84 @@ RSpec.shared_examples ".load" do
 
   subject { described_class.load(json_node) }
 end
+
+RSpec.shared_examples "JSON field" do |method: , json_key: , required: false, value: nil, map: nil|
+  context "\"#{json_key}\"" do
+    let(:json_value) { json_node[json_key] }
+
+    if value then let(:expected,&value)
+    else
+      if map then let(:expected) { map[json_value] }
+      else        let(:expected) { json_value }
+      end
+    end
+
+    if required
+      it "must set ##{method}" do
+        expect(subject.send(method)).to eq(expected)
+      end
+
+      context "when the \"#{json_key}\" key is missing" do
+        before { json_node.delete(json_key) }
+
+        it do
+          expect {
+            described_class.load(json_node)
+          }.to raise_error(KeyError)
+        end
+      end
+    else
+      context "when the \"#{json_key}\" key is present" do
+        it "must set ##{method}" do
+          expect(subject.send(method)).to eq(expected)
+        end
+      end
+
+      context "when the \"#{json_key}\" key is missing" do
+        before { json_node.delete(json_key) }
+
+        it do
+          expect(subject.send(method)).to be(nil)
+        end
+      end
+    end
+  end
+end
+
+RSpec.shared_examples "JSON Array field" do |method: , json_key: , required: false, element_class: |
+  let(:json_value) { json_node[json_key] }
+
+  if required
+    it { expect(subject.send(method)).to be_kind_of(Array) }
+    it { expect(subject.send(method)).to_not be_empty }
+    it { expect(subject.send(method)).to all(be_kind_of(element_class)) }
+
+    it "must parse every element of \"#{json_key}\"" do
+      expect(subject.send(method).length).to eq(json_value.length)
+    end
+
+    context "when the \"#{json_key}\" key is missing" do
+      before { json_node.delete(json_key) }
+
+      it do
+        expect {
+          described_class.load(json_node)
+        }.to raise_error(KeyError)
+      end
+    end
+  else
+    context 'when the "CVE_Items" key is present' do
+      it { expect(subject.send(method)).to be_kind_of(Array) }
+      it { expect(subject.send(method)).to_not be_empty }
+      it { expect(subject.send(method)).to all(be_kind_of(element_class)) }
+    end
+
+    context "when the \"#{json_key}\" key is missing" do
+      before { json_node.delete(json_key) }
+
+      it do
+        expect(subject.send(method)).to eq([])
+      end
+    end
+  end
+end
